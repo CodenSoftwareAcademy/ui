68a28b82752f13fa9a64e0692aeb2d92dab9fe0b
cosminandreicontu <cosminandrei.cnt@gmail.com>
2020-06-03 21:36:31 +0300

    Updates

---
+++ precache-manifest.26293dadbe5c57ef6a7a9f1bd304b0f1.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a357f3f94b3194d88ab4f40c89dab473",
+    "url": "/ui/index.html"
+  },
+  {
+    "revision": "8847a14a26c068757686",
+    "url": "/ui/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "653802e1b63a21c315ed",
+    "url": "/ui/static/css/main.ddd5b899.chunk.css"
+  },
+  {
+    "revision": "8847a14a26c068757686",
+    "url": "/ui/static/js/2.297157c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ui/static/js/2.297157c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "653802e1b63a21c315ed",
+    "url": "/ui/static/js/main.023d0bf7.chunk.js"
+  },
+  {
+    "revision": "0816e56d455e57dc4478",
+    "url": "/ui/static/js/runtime-main.531d3e72.js"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/ui/static/media/Redux.4912f6d8.png"
+  },
+  {
+    "revision": "e38178f98e6e53f5b9aeaaec7024eba4",
+    "url": "/ui/static/media/backend.e38178f9.png"
+  },
+  {
+    "revision": "50fa78ba65ff7b37683ffe25fcdd5275",
+    "url": "/ui/static/media/bullet.50fa78ba.png"
+  },
+  {
+    "revision": "5cb048894b98824ca8398903281b7522",
+    "url": "/ui/static/media/bullet11.5cb04889.png"
+  },
+  {
+    "revision": "6c90939f465e7513538f6d156df4d411",
+    "url": "/ui/static/media/bullet2.6c90939f.png"
+  },
+  {
+    "revision": "9d0d03cde0ea5de24c1452b9092bee9f",
+    "url": "/ui/static/media/bullet31.9d0d03cd.png"
+  },
+  {
+    "revision": "ec54b4ff1198a377a4b8bb1f5bed0885",
+    "url": "/ui/static/media/cover4.ec54b4ff.png"
+  },
+  {
+    "revision": "d0a5a799ae37a795c9d741e0bd9c5d11",
+    "url": "/ui/static/media/csa.d0a5a799.png"
+  },
+  {
+    "revision": "c26a73b45b9a801864f13edeb0aa2cfb",
+    "url": "/ui/static/media/css.c26a73b4.png"
+  },
+  {
+    "revision": "98ff4822d03ab0f5746653f56441d38e",
+    "url": "/ui/static/media/express.98ff4822.png"
+  },
+  {
+    "revision": "4dd64e74c8ab181680d3c89d61cfd074",
+    "url": "/ui/static/media/frontend.4dd64e74.png"
+  },
+  {
+    "revision": "3453e70f7a39df77082d6c8b3a9d7af0",
+    "url": "/ui/static/media/gen11.3453e70f.png"
+  },
+  {
+    "revision": "00a32b362897604414d577ac8fccdacb",
+    "url": "/ui/static/media/gen21.00a32b36.png"
+  },
+  {
+    "revision": "4b3602d72c2d07cfc6e3ad4f885e9b3c",
+    "url": "/ui/static/media/git.4b3602d7.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/ui/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "db7933e6a7f7acaf99a33f39b286974b",
+    "url": "/ui/static/media/js.db7933e6.png"
+  },
+  {
+    "revision": "16ec7689685b9a3190d9640e10975c4d",
+    "url": "/ui/static/media/jwt.16ec7689.svg"
+  },
+  {
+    "revision": "09ed2051ed0b6e784bede6e70c64e9e7",
+    "url": "/ui/static/media/mongodb.09ed2051.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/ui/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/ui/static/media/node.521444a2.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/ui/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "5140ce702a62703fa7bb4c3573c3aeb8",
+    "url": "/ui/static/media/week-ro.5140ce70.png"
+  }
+]);
